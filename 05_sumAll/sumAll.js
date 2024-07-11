75b38b8dd58b104319574b84d1a291fb18b2e1ab
Minh Duc Nguyen <126580562+mhndckngyn@users.noreply.github.com>
2024-07-11 16:07:23 +0700

    Add solution for problem 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+  if (a < 0 || b < 0) return "ERROR";
 
+  let startNumber = Math.min(a, b);
+  let endNumber = Math.max(a, b);
+  let sum = 0;
+  for (startNumber; startNumber <= endNumber; startNumber++) sum += startNumber;
+  return sum;
 };
 
 // Do not edit below this line
