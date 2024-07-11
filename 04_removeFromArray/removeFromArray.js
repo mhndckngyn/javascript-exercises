d58de162fac240e8588a24e052e1446e23d1630b
Minh Duc Nguyen <126580562+mhndckngyn@users.noreply.github.com>
2024-07-11 15:44:09 +0700

    Add solution for problem 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
+    const newArray = array.filter( item => !itemsToRemove.includes(item))
+    return newArray;
 };
 
 // Do not edit below this line
