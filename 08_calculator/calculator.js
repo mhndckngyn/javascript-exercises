7e37bb6056360cba87ac4cc13b7109783cd7dc88
Minh Duc Nguyen <126580562+mhndckngyn@users.noreply.github.com>
2024-07-20 17:05:11 +0700

    Add solution for problem 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, current) => total += current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => total *= current, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  for (let i = num; i >= 2; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
