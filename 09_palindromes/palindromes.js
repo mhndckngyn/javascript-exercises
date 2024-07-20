a2633f8b0ecf0ed8f8847621760d4ba38eeb3a8a
Minh Duc Nguyen <126580562+mhndckngyn@users.noreply.github.com>
2024-07-20 18:50:45 +0700

    Adjust solution for problem 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,20 +1,30 @@
 const standardize = function (str) {
-  const NON_ALPHANUMERIC = /[^a-zA-Z0-9]/g;
-  return str.replaceAll(NON_ALPHANUMERIC, "").toLowerCase();
+  // const NON_ALPHANUMERIC = /[^a-zA-Z0-9]/g;
+  // return str.replaceAll(NON_ALPHANUMERIC, "").toLowerCase();
+  const ALPHANUMERIC = "0123456789abcdefghikjlmnopqrstuvwxyz";
+  return str
+    .toLowerCase()
+    .split("")
+    .filter((char) => ALPHANUMERIC.includes(char))
+    .join("");
 };
 
 const palindromes = function (str) {
   str = standardize(str);
 
-  let i = 0,
-    j = -1;
-  while (i <= Math.floor(str.length / 2)) {
-    if (str.at(i) !== str.at(j)) return false;
-    i++;
-    j--;
-  }
+  const reversed = str.split("").reverse().join("");
+  return reversed === str;
+  
 
-  return true;
+  // let i = 0,
+  //   j = -1;
+  // while (i <= Math.floor(str.length / 2)) {
+  //   if (str.at(i) !== str.at(j)) return false;
+  //   i++;
+  //   j--;
+  // }
+
+  // return true;
 };
 
 // Do not edit below this line
