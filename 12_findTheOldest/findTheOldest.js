d7ffeb196d5c96b175193e56ab501e63fc508455
Minh Duc Nguyen <126580562+mhndckngyn@users.noreply.github.com>
2024-07-20 18:08:35 +0700

    Add solution for problem 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function(peopleList) {
+const getAge = function(person) {
+  if ("yearOfDeath" in person)
+    return person.yearOfDeath - person.yearOfBirth;
   
+  let currentYear = new Date().getFullYear();
+  return currentYear - person.yearOfBirth;
+}
+
+const findTheOldest = function(personList) {
+  return personList.reduce((oldest, current) => {
+    const oldestAge = getAge(oldest);
+    const currentAge = getAge(current);
+
+    return oldestAge > currentAge ? oldest : current;
+  })
 };
 
 // Do not edit below this line
