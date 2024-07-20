66c7603f3bcdd199bb06964518d541d3e3de0f59
Minh Duc Nguyen <126580562+mhndckngyn@users.noreply.github.com>
2024-07-20 17:41:18 +0700

    Add solution for problem 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,14 @@
-const getTheTitles = function() {
+const getTheTitles = function (bookList) {
+  // return bookList.reduce((nameList, current) => {
+  //   nameList.push(current.title);
+  //   return nameList;
+  // }, []);
 
+  const nameList = [];
+  for (book of bookList) {
+    nameList.push(book.title);
+  }
+  return nameList;
 };
 
 // Do not edit below this line
