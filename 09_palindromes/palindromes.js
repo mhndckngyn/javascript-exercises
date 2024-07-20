21180a2876ffa3d344bf94d8b7e876d1bda8900c
Minh Duc Nguyen <126580562+mhndckngyn@users.noreply.github.com>
2024-07-20 18:44:10 +0700

    Add solution for problem 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const standardize = function (str) {
+  const NON_ALPHANUMERIC = /[^a-zA-Z0-9]/g;
+  return str.replaceAll(NON_ALPHANUMERIC, "").toLowerCase();
+};
 
+const palindromes = function (str) {
+  str = standardize(str);
+
+  let i = 0,
+    j = -1;
+  while (i <= Math.floor(str.length / 2)) {
+    if (str.at(i) !== str.at(j)) return false;
+    i++;
+    j--;
+  }
+
+  return true;
 };
 
 // Do not edit below this line
